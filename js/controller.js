b9a64349f5e5ee45401e4315ebfc6efdea68dd14
trompot.nara <NAKNAO-nnct@users.noreply.github.com>
2018-07-03 00:45:41 +0900

    追記

--- js/controller.js
+++ js/controller.js
@@ -1,28 +1,28 @@
-$(function () {
-    $(document).keydown(function (event) {
-        // クリックされたキーコードを取得する
-        var keyCode = event.keyCode;
-        // F5 の場合は falseをリターン
-        if (keyCode == 40) {
-            nextPage();
-        }
-    });
-});
+// $(function () {
+//     $(document).keydown(function (event) {
+//         // クリックされたキーコードを取得する
+//         var keyCode = event.keyCode;
+//         // F5 の場合は falseをリターン
+//         if (keyCode == 40) {
+//             nextPage();
+//         }
+//     });
+// });
 
-var page = ["page1", "page2-1", "page2-2", "page2-3"];
-// var i = 0;
-function nextPage(){
-    // query
-    var url = decodeURI(location.href);
-    var getPage = url.split("#");
-    var page_index = 0;
-    if (getPage[1] != undefined){
-        page_index = page.indexOf(getPage);
-        console.log(page_index);
-    }
-    console.log(getPage[1]);
-    $(page[page_index]).addClass("nonono");
-    $(page[page_index + 1]).removeClass("nonono");
-    location.href =  "#" + getPage[page_index + 1];
+// var page = ["page1", "page2-1", "page2-2", "page2-3"];
+// // var i = 0;
+// function nextPage(){
+//     // query
+//     var url = decodeURI(location.href);
+//     var getPage = url.split("#");
+//     var page_index = 0;
+//     if (getPage[1] != undefined){
+//         page_index = page.indexOf(getPage);
+//         console.log(page_index);
+//     }
+//     console.log(getPage[1]);
+//     $(page[page_index]).addClass("nonono");
+//     $(page[page_index + 1]).removeClass("nonono");
+//     location.href =  "#" + getPage[page_index + 1];
 
-}
+// }
