7db8e10f1f7a6a05bbf8c0f702634b236e6a802a
trompot.nara <NAKNAO-nnct@users.noreply.github.com>
2018-07-05 01:38:39 +0900

    Add files via upload

---
+++ js/deffine.js
@@ -0,0 +1,5 @@
+// socket url
+var socket_url = "http://media.trompot.mydns.jp:18270/";
+
+// socket.io
+var socket_io = socket_url + 'socket.io/socket.io.js';
--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@
 var fs = require("fs");
 var server = require("http").createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    var output = fs.readFileSync("./chat.html", "utf-8");
+    var output = fs.readFileSync("../index.html", "utf-8");
     res.end(output);
-}).listen(9500);  // portを指定
+}).listen(18270);  // portを指定
 
 var io = require("socket.io").listen(server);
 
